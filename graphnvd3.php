ae8ccd7fe1495e4a78b4d4821d3738085a29f6d3
Jerome Nadaud <jerome.nadaud@gmail.com>
2014-06-02 09:08:23 +0200

    //Version update

--- graphnvd3.php
+++ graphnvd3.php
@@ -42,7 +42,7 @@ class GraphNvD3 extends ModuleGraphEngine
 
 		$this->name = 'graphnvd3';
 		$this->tab = 'administration';
-		$this->version = '1.2';
+		$this->version = '1.3';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
